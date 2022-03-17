4cb45eb71f14045e4c46c15dcbc34954cdd9b62e
Sarosis <gustimationsarosis@gmail.com>
2022-03-17 08:49:43 +0000

    Fix to stop clicking on tooltip

--- DragUtils.js
+++ DragUtils.js
@@ -5,11 +5,6 @@ function dragEnd(ev) {
     updateCompSelection();
 }
 
-//todo: on drag set size to 0, and drop reset size 
-//https://jsfiddle.net/cshao/cyhq3dqo/8/
-
-
-
 function selectPlayerCLass(ev) {
     var playerId = ev.target.id;
     var playerType = ev.target.getAttribute('data-type');
@@ -23,7 +18,7 @@ function selectPlayerCLass(ev) {
         for (var ii = 0; ii < tableRows[i].cells.length; ii++) {
             if (tableRows[i].cells[ii].textContent == "") {
                 nextEmptyySlot = tableRows[i].cells[ii].id;
-            break;
+                break;
             }
         }
         if (nextEmptyySlot != "")
@@ -44,13 +39,11 @@ function selectPlayerCLass(ev) {
 }
 
 function drop(ev, onLoad) {
-
     var playerDescription = "";
     var playerId = "";
     var playerId2;
     var fe = "" + fromEvent;
     var fromRaid;
-    var playerBuffs;
 
     if (ev != null) {
         ev.preventDefault();
@@ -58,13 +51,7 @@ function drop(ev, onLoad) {
         playerId = ev.target.id;
         fromRaid = document.getElementById(fe).innerText
         playerId2 = document.getElementById(playerDescription);
-        playerBuffs = PlayerType(playerId2.getAttribute('data-rid')).raidBuffs;
-
-        var tip = document.getElementById("" + PlayerType(playerId2.id).id + 'tooltip');
-        tip.style.height = 'auto';
-        tip.style.display = 'block';
-        tip.style.padding = '5px 0 5px 0';
-
+        showHideToolTip(true, PlayerType(playerId2.id).id)
     }
     else {
         playerDescription = onLoad.player;
@@ -78,8 +65,10 @@ function drop(ev, onLoad) {
         var player = document.getElementById(playerDescription);
         var targetGridSlot = document.getElementById(playerId);
         targetGridSlot.style.backgroundColor = player.style.backgroundColor;
+        // Inner text contains the span
         var playerName = innerText = player.innerText.split('\n');
-        targetGridSlot.innerText = playerName[playerName.length -1];
+
+        targetGridSlot.innerText = playerName[playerName.length - 1];
         targetGridSlot.setAttribute('data-type', player.getAttribute('data-type'))
         targetGridSlot.setAttribute('data-rid', player.getAttribute('data-rid'))
     }
@@ -102,31 +91,41 @@ function clearRaidBox(id) {
 }
 
 function mousedown(ev) {
-    var test = ev.target.getAttribute('data-rid');
-    var tip = document.getElementById("" + test + 'tooltip');
-    tip.style.height = "0";
-    tip.style.display = 'none';
-    tip.style.padding = '0 0 0 0';
+    var toolTip = ev.target.getAttribute('data-rid');
+    if (toolTip == null) {
+        return;
+    }
+    showHideToolTip(false, toolTip)
 }
 
 function mouseup(ev) {
-
-    var test = ev.target.getAttribute('data-rid');
-    var tip = document.getElementById("" + test + 'tooltip');
-    tip.style.height = 'auto';
-    tip.style.display = 'block';
-    tip.style.padding = '5px 0 5px 0';
+    var toolTip = ev.target.getAttribute('data-rid');
+    if (toolTip == null) {
+        return;
+    }
+    showHideToolTip(true, toolTip)
 }
 
 function drag(ev) {
-
-
     ev.dataTransfer.setData("text", ev.target.id);
     fromEvent = ev.target.id;
-
-
 }
 
 function allowDrop(ev) {
     ev.preventDefault();
+}
+
+function showHideToolTip(show, toolTip) {
+    var tip = document.getElementById("" + toolTip + 'tooltip');
+
+    if (show) {
+        tip.style.height = 'auto';
+        tip.style.display = 'block';
+        tip.style.padding = '5px 0 5px 0';
+    }
+    else {
+        tip.style.height = "0";
+        tip.style.display = 'none';
+        tip.style.padding = '0 0 0 0';
+    }
 }
