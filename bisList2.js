39b075df0f89d563ef33e7d5c632ccc04d335c41
Sarosis <gustimationsarosis@gmail.com>
2022-04-14 21:42:37 +0100

    coupled spec select

--- bisList2.js
+++ bisList2.js
@@ -9,16 +9,12 @@ function myFunction(event, num) {
     else
         event.target.innerHTML = " < ";
 
-    var test = event;
     console.log(event.target.innerHTML);
 }
-//player selecttor
+
+//player selector
 function myFunction2(event, num) {
-    var dropDown = document.getElementById("myDropdown" + num).classList.toggle("show");
-
-
-    var test = event;
-    console.log(event.target.innerHTML);
+    document.getElementById("myDropdown" + num).classList.toggle("show");
 }
 
 function filterValues() {
@@ -35,6 +31,44 @@ function selectNewClass(event) {
     classSpec = classSpec[classSpecData];
     model = classSpec;
     buildItemTable();
+    setSpecDropDown(classNameData);
+}
+
+function setSpecDropDown(classNameData) {
+    var specList = document.getElementById('specList');
+    specList.innerHTML = "";
+    var itemDearder;
+
+    const test = classTypes[classNameData];
+    var index = Object.keys(test).length;
+    for (var i = 0; i < index; i++) {
+        var test2 = Object.keys(test)[i];
+
+        if (i == 0) {
+            itemDearder = specList.appendChild(document.createElement('div'));
+            itemDearder.classList.add('dropitem');
+            var itemContainer = itemDearder.appendChild(document.createElement('div'));
+            itemContainer.classList.add('container', 'space-between');
+            var a = itemContainer.appendChild(document.createElement('a'));
+            a.setAttribute('data-class', Object.keys(test)[i]);
+            a.innerHTML = Object.keys(test)[i];
+            continue;
+        }
+        else if (i == 1) {
+            itemDearder = specList.appendChild(document.createElement('div'));
+            itemDearder.classList.add('dropdown-content');
+            itemDearder.id = "myDropdown4";
+            itemDearder.classList.add('dropDownSelectList');
+        }
+
+        var itemContainer = itemDearder.appendChild(document.createElement('div'));
+        itemContainer.classList.add('itemSelectBtn');
+        var a = itemContainer.appendChild(document.createElement('a'));
+        a.setAttribute('data-class', Object.keys(test)[i]);
+        a.innerHTML = Object.keys(test)[i];
+
+    //    var test = model;
+    }
 }
 
 function setFilters(event) {
@@ -45,7 +79,6 @@ function setFilters(event) {
         event.value = true;
     filterValues();
     buildItemTable();
-
 }
 
 function buildItemTable() {
@@ -53,7 +86,6 @@ function buildItemTable() {
     var table = document.getElementById('itemTable');
     table.innerHTML = "";
 
-    var test = model.head;
     var itemContainerI = 0;
     for (var key in model) {
         var dropDown = document.createElement('div');
@@ -66,7 +98,6 @@ function buildItemTable() {
             if (items[i][1] == "Nax" && naxFilter == "true") {
                 continue;
             }
-            var test2 = items[i];
             if (itemContainerI == 0) { //first item
                 dropItem = document.createElement('div');
                 dropItem.classList.add('dropitem');
@@ -79,23 +110,18 @@ function buildItemTable() {
                 itemContainerI++;
             }
 
-
             var itemListContainer = document.createElement('div');
+
             itemListContainer.classList.add('container', 'space-between');
-
             createItem(itemListContainer, items[i], key, itemCounter);
-
-
             dropItem.appendChild(itemListContainer);
             dropDown.appendChild(dropItem)
             table.appendChild(dropDown);
             itemCounter++;
 
         }
-
     }
     $WowheadPower.refreshLinks();
-
 }
 
 function createItem(itemListContainer, item, key, i) {
@@ -107,7 +133,7 @@ function createItem(itemListContainer, item, key, i) {
     aitem.href = "#";
     aitem.rel = "item=" + item[0];
     aitem.innerHTML = "";
-    aitem.style.width = "280px";
+    aitem.style.width = "250px";
     itemListContainer.appendChild(aitem);
     var aitem = document.createElement('a');
     aitem.innerHTML = item[1];
@@ -128,7 +154,7 @@ function createItem(itemListContainer, item, key, i) {
     if (i == 0) {
         var btn = document.createElement('button');
         btn.classList.add('dropbtn');
-        btn.setAttribute('onclick', 'myFunction(event,"' + key +'")');
+        btn.setAttribute('onclick', 'myFunction(event,"' + key + '")');
         btn.innerHTML = "<";
         itemListContainer.appendChild(btn);
     } else {
